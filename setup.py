47ef3fd6295a13b514610d82ce11b3573d5235d2
Jakub Wilk <jwilk@jwilk.net>
2009-02-22 18:43:23 +0100

    Add a setup script.

---
+++ setup.py
@@ -0,0 +1,35 @@
+'''
+*python-aalibs* is a set of bindings for
+`AAlib <http://aa-project.sourceforge.net/aalib/>`_,
+an ASCII art library.
+'''
+
+classifiers = '''\
+Development Status :: 4 - Beta
+Intended Audience :: Developers
+License :: OSI Approved :: GNU General Public License (GPL)
+Operating System :: OS Independent
+Programming Language :: Python
+Programming Language :: Python :: 2
+Topic :: Multimedia :: Graphics\
+'''.split('\n')
+
+try:
+        from setuptools import setup
+except ImportError:
+        from distutils.core import setup
+
+setup(
+	name = 'python-aalib',
+	version = '0.1',
+	license = 'GNU GPL 2',
+	description = 'Bindings for AAlib',
+	long_description = __doc__.strip(),
+	classifiers = classifiers,
+	url = 'http://jwilk.nfshost.com/software/python-aalib.html',
+	author = 'Jakub Wilk',
+	author_email = 'ubanus@users.sf.net',
+	py_modules = ['aalib']
+)
+
+# vim:ts=4 sw=4 et
