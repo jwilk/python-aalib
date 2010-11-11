47b2a10c5da1b6ca63b8d68e4c9cc40e7fe6cbf0
Jakub Wilk <jwilk@jwilk.net>
2010-11-11 23:54:18 +0100

    test: use LinuxScreen is possible.

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
+import os
 import urllib2
 from cStringIO import StringIO
 
 import Image
 import aalib
 
-screen = aalib.AnsiScreen(width=60, height=30)
+if os.getenv('TERM') == 'linux':
+    screen = aalib.LinuxScreen
+else:
+    screen = aalib.AnsiScreen
+screen = screen(width=60, height=30)
 fp = StringIO(urllib2.urlopen('http://python.org/favicon.ico').read())
 image = Image.open(fp).convert('L').resize(screen.virtual_size)
 screen.put_image((0, 0), image)
