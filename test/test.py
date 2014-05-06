a63ff2729073756b5ac448480b8fe5f2952ce9b9
Jakub Wilk <jwilk@jwilk.net>
2014-05-06 11:27:11 +0200

    test: make it work offline.

--- test/test.py
+++ test/test.py
@@ -14,17 +14,21 @@ except ImportError:
     # Python 3.X
     from io import BytesIO
 
-from PIL import Image
+from PIL import Image, ImageOps
 import aalib
+
+here = os.path.dirname(__file__)
 
 if os.getenv('TERM') == 'linux':
     screen = aalib.LinuxScreen
 else:
     screen = aalib.AnsiScreen
-screen = screen(width=60, height=30)
-fp = BytesIO(urlopen('http://python.org/favicon.ico').read())
-image = Image.open(fp).convert('L').resize(screen.virtual_size)
-screen.put_image((0, 0), image)
-print(screen.render())
+screen = screen(width=76, height=24)
+with open(os.path.join(here, 'python.jpeg'), 'rb') as fp:
+    image = Image.open(fp).convert('L')
+    image = ImageOps.invert(image)
+    image = image.resize(screen.virtual_size)
+    screen.put_image((0, 0), image)
+    print(screen.render())
 
 # vim:ts=4 sw=4 et
